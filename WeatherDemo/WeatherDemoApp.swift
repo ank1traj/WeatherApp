b18e7723a0d1b08a2938514bdd821d6d1885de28
Ankit Raj <ankit.raj928@gmail.com>
2022-04-03 16:45:39 +0530

    Add files via upload

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Ankit Raj on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
